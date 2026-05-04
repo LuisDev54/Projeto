624ac8709c71eb99761bcac6c9838e5508e7b81c
LuisDev54 <luisao3por4@gmail.com>
2026-05-04 14:59:54 -0300

    Atualizar o p.py

--- p.py
+++ p.py
@@ -1,3 +1,31 @@
 # login
+usuario = []
+animais = []
+produtos = []
+# qualquer coisa a gente add mais listas
+id_produto = 1
+#contador de produtos !
+login_do_usuario = 0
+perfil-do-login = 0
+
+while true:
+    print("-----Bem vindo Agroapp -----")
+    print("1 - login do usuario !")
+    print("2 - login adm !")
+    print("3 - cadastro do usuario !")
+    print("4 - alterar produto " )
+    print("5 - Sair ")
+
+    if usuario == "adm":
+        print("---adm_logado---")
+        print("1- cadastrar produto")
+        #vamos colocar uma .apped para essa função
+        print("2 - remover produto !")
+        # vamos usar um .pop para essa função
+        print("3 - adicionar animal")
+        print("4 - remover animal")
+        print("5 -  ")
+        print("- sair")
+
 # cliente
 # admin
