e26ade73b71c07e13ee7e79a761c58e83fa4f20c
LuisDev54 <luisao3por4@gmail.com>
2026-05-10 20:28:43 -0300

    Update p.py

--- p.py
+++ p.py
@@ -1,14 +1,14 @@
  # login
-Usuario_ADM = [
+usuarioADM = [
 ["wallisson", "wallisson123@gmail.com", "#agronegócio"], ["guilherme", "guigui@gmail.com", "@algoritmos"], 
 ["renê", "gadelha@gmail.com", "coordenadoria26"], ["gilvan", "reigil@gmail.com", "gilvanII"]
 ]
 
-Usuario_CLI = [
+usuarioCLI = [
 ["gabriel", "gabrielbastos@gmail.com", "zecabode"], ["zacarias", "zacarias01@gmail.com", "fazendajatobar###"]
 ]
 
-Usuarios = [Usuario_ADM, Usuario_CLI]
+usuarios = [usuarioADM, usuarioCLI]
 
 animais = ["Bovinos", "Ovinos", "Caprinos", "Aves", "Equinos", "Suinos"]
 produtos = []
@@ -20,14 +20,14 @@ login_do_usuario = 0
 perfil_do_login = 0
 
 while True:
-    print("----- Bem vindo ao Agro app -----")
+    print("------ Bem vindo ao Agro app ------")
     print("1 - login do usuario" )
     print("2 - cadastro de usuario")
     print("3 - Sair")
     op = int(input(""))
     if op == 1:
         while True:
-            nome = input("Nome: ")
+            nome = input("Nome: ").strip()
 
             tem_numero = False
             i = 0
@@ -45,17 +45,16 @@ while True:
             else:
                 break
         while True:
-            email = input("Email: ")
-            if email == "":
+            email = input("Email: ").strip()
+            if "@" not in email:
                 print("Email inválido!")
-            elif "@" not in email:
-                print("Email inválido!")
-            elif "gmail.com" not in email:
-                print("Email inválido!")
+            elif "gmail.com" not in email and "hotmail.com" not in email:
+                print("Email inválido! # 1")
             elif email[0] == "@":
                 print("Email inválido!")
+            elif email[-9:] != "gmail.com" and email[-11:] != "hotmail.com":
+                print("Email inválido! # 2")
             else:
-                print("Email válido!")
                 break
         
         while True:
@@ -64,10 +63,21 @@ while True:
                 break
             else:
                 print("Senha inválida! Mínimo 8 caracteres.")
-    
+        while True: 
+            for e in usuarios:
+                if [nome, email, senha] in usuarioADM:
+                    print("test")
+                    break
+                elif [nome, email, senha] in usuarioCLI:
+                    print("test")
+                    break
+                elif [nome, email, senha] not in usuarioCLI or usuarioADM:
+                    print("usuário não encontrado.")
+                    break #Break n ta funcionando 
+
     elif op == 2:
         while True:
-            nome = input("Nome: ")
+            nome = input("Nome: ").strip()
 
             tem_numero = False
             i = 0
@@ -85,35 +95,34 @@ while True:
             else:
                 break
         while True:
-            email = input("Email: ")
-            if email == "":
+            email = input("Email: ").strip()
+            if "@" not in email:
                 print("Email inválido!")
-            elif "@" not in email:
-                print("Email inválido!")
-            elif "gmail.com" not in email:
-                print("Email inválido!")
+            elif "gmail.com" not in email and "hotmail.com" not in email:
+                print("Email inválido! # 1")
             elif email[0] == "@":
                 print("Email inválido!")
+            elif email[-9:] != "gmail.com" and email[-11:] != "hotmail.com":
+                print("Email inválido! # 2")
             else:
-                print("Email válido!")
                 break
         
         while True:
-            senha = input("Senha: ")
+            senha = input("Senha: ").strip()
             if len(senha) >= 8:
                 break
             else:
                 print("Senha inválida! Mínimo 8 caracteres.")
         while True:
-            perfil = input("Perfil (adm/cliente): ")
+            perfil = input("Perfil (adm/cliente): ").strip().lower()
 
             if perfil == "adm":
-                Usuario_ADM.append([nome, email, senha])
+                usuarioADM.append([nome, email, senha]).lower()
                 print("Usuário cadastrado com sucesso.")
                 break  
 
             elif perfil == "cliente":
-                Usuario_CLI.append([nome, email, senha])
+                usuarioCLI.append([nome, email, senha]).lower()
                 print("Usuário cadastrado com sucesso.")
                 break 
 
@@ -122,6 +131,7 @@ while True:
 
     elif op == 3:
         print("Saindo...")
+        print("volte sempre!")
         break
     else:
         print("Comando Inválido, tente novamente.")
